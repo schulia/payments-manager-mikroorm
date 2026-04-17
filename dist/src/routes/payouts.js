6adb07d886ef00f17aa02b713a255b249d2bc5a7
Julia <julia.schefer@spirii.com>
2026-04-17 12:35:55 +0200

    gitignore

--- dist/src/routes/payouts.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import * as payoutController from '../controllers/payouts.js';
-const router = Router();
-// router.get('/:id', payoutController.getPayoutById);
-router.post('/', payoutController.createPayout);
-router.get('/', payoutController.getPayouts);
-// router.get('/users/:userId', payoutController.getPayoutByUserId);
-// router.get('/transaction/:transactionId', payoutController.getPayoutByTransactionId);
-export default router;
--- dist/src/routes/users.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import * as userController from '../controllers/users.js';
-const router = Router();
-router.get('/', userController.getUsers);
-router.post('/', userController.createUser);
-// router.get('/:userId/balance', userController.getUserBalance);
-export default router;
