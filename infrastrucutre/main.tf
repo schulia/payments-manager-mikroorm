a040ce35bea26183aba59fa91639520edd367aaa
Julia <julia.schefer@spirii.com>
2026-05-25 14:01:24 +0200

    plan

---
+++ infrastrucutre/main.tf
@@ -0,0 +1,2 @@
+# asg, security group, launch configuration, and load balancer
+# null resource sets ips and  ssh access to the instances to github secrets
---
+++ infrastrucutre/variables.tf
@@ -0,0 +1,1 @@
+# names and versions of resources
